787718c929c738571d65e853312408a9e48a9b98
Wilfried                                     Le Cor <wilfried.lecor@gmail.com>
2014-03-24 13:33:48 +0100

    Create State.cpp

---
+++ StateMachine/State.cpp
@@ -0,0 +1,27 @@
+#include "State.hpp"
+#include "StateMachine.hpp"
+
+State::State(StateMachine &states, Application *app) :
+    mStateMachine(&states),
+    mApp(app)
+{
+}
+
+State::~State()
+{
+}
+
+void State::requestStackPush(StateIdentifier stateID)
+{
+    mStateMachine->pushState(stateID);
+}
+
+void State::requestStackPop()
+{
+    mStateMachine->popState();
+}
+
+void State::requestStackClear()
+{
+    mStateMachine->clearStates();
+}
